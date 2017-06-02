954a9a6a1b7df9ff6ec68b6766b6e118bfc7304e
Raghu Rana <Raghu Rana>
2017-06-02 22:32:53 +1000

    Using Observable.create, a low level api

--- main.ts
+++ main.ts
@@ -2,7 +2,17 @@
 import { Observable, Observer } from 'rxjs';
 
 let numbers = [1, 5, 10];
-let source  = Observable.from(numbers);
+let source  = Observable.create(observer => {
+
+    for(let n of numbers) {
+        if(n === 5) 
+            observer.error("Bad value 5");
+
+        observer.next(n);    
+    }
+
+    observer.complete();
+})
 
 class MyObserver implements Observer<number>  {
     
