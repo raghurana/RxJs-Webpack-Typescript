70aebf9af4cbc67d42899779f41b7deffc42090f
Raghu Rana <Raghu Rana>
2017-06-24 12:25:36 +1000

    Observerable.from and create methods are done.

--- main.ts
+++ main.ts
@@ -1,38 +1,48 @@
+import {Observable, Observer} from 'rxjs';
 
-import { Observable, Observer } from 'rxjs';
+const completedString = "completed\r\n--------";
 
 let numbers = [1, 5, 10];
-let source  = Observable.create(observer => {
+let source = Observable.from(numbers);
 
-    for(let n of numbers) {
-        if(n === 5) 
-            observer.error("Bad value 5");
-
-        observer.next(n);    
-    }
-
-    observer.complete();
-})
-
-class MyObserver implements Observer<number>  {
-    
-    next(val){
-        console.log(`value: ${val}`);        
+class MyObserver implements Observer<number> {
+    next(data: number) {
+        console.log(data);
     }
 
     error(e) {
-        console.log(`error: ${e}`);
+        console.log(e);
     }
 
     complete() {
-        console.log("complete");
+        console.log(completedString); 
     }
-
 }
 
+// Using strongly type custom oberver:
+console.log("Using strongly typed observer...")
 source.subscribe(new MyObserver());
+
+// Using 3 NEC arrow functions
+console.log("Using NEC arrow functions...") 
 source.subscribe(
-    val => console.log(`value from arrow func: ${val}`),
-    e => console.log(`error from arrow func: ${e}`),
-    () => console.log("complete from arrow func")
-);
+    data => console.log(data), 
+    e => console.log(e), 
+    () => console.log(completedString));
+
+
+let source2 = Observable.create(observer => {
+    for(let n of numbers) { 
+        if(n === 5)
+            observer.error("Oops");
+            
+       observer.next(n);
+    }
+    observer.complete();
+});
+
+console.log("using observable.create..")
+source2.subscribe(
+    data => console.log(data), 
+    e => console.log(e), 
+    () => console.log(completedString));
